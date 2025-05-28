5ba8ad72213e35aacc550f1a87989ec9ca6df5aa
bohdanhor <bogdangor12@gmail.com>
2025-05-28 14:53:28 +0100

    just a boiler

---
+++ script.js
@@ -0,0 +1,18 @@
+let bulbasaur = document.getElementById("bulbasaur-section");
+let charmander = document.getElementById("charmander-section");
+
+let clickBulbasaur = document.getElementById("bulbasaur-btn");
+let clickCharmander = document.getElementById("charmander-btn");
+
+
+
+function showSection(sectionId) {
+    // remove active class
+    let sections = document.getElementsByClassName("section");
+
+    for(let i=0; i<sections.length; i++) {
+        sections[i].classList.remove("active");
+    }
+
+    // add active c
+}
