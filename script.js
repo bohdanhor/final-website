82d27d2221a56a03c9b7c6cefcb283697a014536
bohdanhor <bogdangor12@gmail.com>
2025-05-29 03:19:37 +0100

    omg i need to stop doing it at night

--- script.js
+++ script.js
@@ -4,29 +4,30 @@ let charmander = document.getElementById("charmander-section");
 let clickBulbasaur = document.getElementById("bulbasaur-btn");
 let clickCharmander = document.getElementById("charmander-btn");
 
-
-
 function showSection(sectionId) {
-    // remove active class
-    let sections = document.getElementsByClassName("section");
+  // remove active class
+  let sections = document.getElementsByClassName("section");
 
-    for(let i=0; i<sections.length; i++) {
-        sections[i].classList.remove("active");
-    }
+  for (let i = 0; i < sections.length; i++) {
+    sections[i].classList.remove("active");
+  }
 
-    // add active c
+  // add active c
 }
 
-const navBarSect = document.querySelectorAll('.hover-section');
 
-  navBarSect.forEach(section => {
-    section.addEventListener('mouseenter', () => {
-      section.classList.remove('default-style');
-      section.classList.add('hover-style');
-    });
+/* copied it from internet and edited --------------------------------------- START */
+const navBarSect = document.querySelectorAll(".hover-section");
 
-    section.addEventListener('mouseleave', () => {
-      section.classList.remove('hover-style');
-      section.classList.add('default-style');
-    });
+navBarSect.forEach((section) => {
+  section.addEventListener("mouseenter", () => {
+    section.classList.remove("default-style");
+    section.classList.add("hover-style");
   });
+
+  section.addEventListener("mouseleave", () => {
+    section.classList.remove("hover-style");
+    section.classList.add("default-style");
+  });
+});
+/* copied it from internet and edited --------------------------------------- END */
